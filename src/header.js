9ee5ec8611db1d33e497bced4e893e113d36fee0
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-09-15 18:27:47 +0400

    Source building scripts

---
+++ src/header.js
@@ -0,0 +1,8 @@
+/**
+ * This script implements the falling letters game in JavaScript and RightJS
+ *
+ * You can play with the thing over here
+ *   http://stcamp.net/games/t-ninja
+ *
+ * Copyright (C) 2009 Nikolay V. Nemshilov aka St.
+ */
